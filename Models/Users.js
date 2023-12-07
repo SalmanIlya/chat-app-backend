75f1982b1d18dcfccd99c40fed85ce27625868f1
salamanilyas <salamanilyas101@gmail.com>
2023-12-07 18:48:25 +0500

    Create-user-schema

---
+++ Models/Users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    UserName: {
+        type:String,
+        required: true
+    },
+    Email: {
+        type: String,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = User = mongoose.model('User', UserSchema);

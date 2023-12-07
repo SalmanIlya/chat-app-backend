309e05a5601471a27a13a4bad8f7924408e54d07
salamanilyas <salamanilyas101@gmail.com>
2023-12-07 18:50:22 +0500

    create-user-and-login

---
+++ Controllers/User.js
@@ -0,0 +1,68 @@
+const User=require("../Models/Users")
+const dotenv=require("dotenv")
+const CryptoJS=require("crypto-js")
+const bcrypt=require("bcrypt")
+dotenv.config({path:"Config/.env"})
+const SecritKey=process.env.secritkey
+// Register user 
+const createUser=async(req,res)=>{
+    const salt=await bcrypt.genSalt(10)
+    const hashpassword=await bcrypt.hash(req.body.Password,salt)
+    let newUser = new User({
+        UserName:req.body.UserName,
+        Email:req.body.Email,
+        Password:hashpassword
+    });
+    
+  
+    try{
+        await newUser.save()
+        .then(data => {
+            console.log('User created successfully!');
+             res.status(202).json(data)
+        })
+        .catch(err => {
+            console.log('Error occurred while creating a user.');
+            console.log(err);
+         res.status(404).json("error")
+
+        });
+    }catch(error){
+        console.log("Error",error);
+    }
+}
+
+// Login user 
+const LoginUser=async(req,res)=>{
+    try{
+        const {Email,Password} =req.body
+const user=await User.findOne({Email})
+
+if(user){
+ await bcrypt.compare(Password,user.Password,(err,result)=>{
+    if(err){
+        res.status(404).json("password does not match")
+    }else{
+     if(result){
+        const {Password,...other}=user._doc
+        res.status(202).json(other)
+     }
+    }
+})
+  
+ 
+  
+
+}else{
+    res.status(404).json({massage:"user not found"})
+    
+}
+    }catch(err){
+        res.status(404).json({Error :err})
+        console.log(err);
+    }
+}
+module.exports={
+    createUser,
+    LoginUser
+}

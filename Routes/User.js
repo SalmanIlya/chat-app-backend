918b4b4399756da359e7a8bfd648aab4c7a9362c
salamanilyas <salamanilyas101@gmail.com>
2023-12-07 20:46:35 +0500

    add-update-route

--- Routes/User.js
+++ Routes/User.js
@@ -1,8 +1,12 @@
 const express=require("express")
+const Auth=require("../Controllers/Auth")
 const User=require("../Controllers/User")
+
 const app=express.Router()
 
-app.post("/create",User.createUser)
+app.post("/create",Auth.createUser)
 
-app.post("/login",User.LoginUser)
+app.post("/login",Auth.LoginUser)
+
+app.put("/update/:id",User.updateUser)
 module.exports=app

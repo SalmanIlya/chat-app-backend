2fa0a0683a83a7a38f914faf435a82dcccb49afc
salamanilyas <salamanilyas101@gmail.com>
2023-12-07 21:26:15 +0500

    add-api

--- Routes/User.js
+++ Routes/User.js
@@ -9,4 +9,8 @@ app.post("/create",Auth.createUser)
 app.post("/login",Auth.LoginUser)
 
 app.put("/update/:id",User.updateUser)
+
+app.get("/singleuser/:id",User.GetSingleUser)
+
+app.get("/alluser",User.getAllUser)
 module.exports=app

76f175bf4d444b1531635dac6a317457a84cd9e8
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 16:41:10 +0500

    small-updates

--- Database/Db.js
+++ Database/Db.js
@@ -1,9 +1,16 @@
 const mongoose=require("mongoose")
 const  db=()=>{
-    mongoose.connect(process.env.db).then(()=>{
+    mongoose.connect(process.env.db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+        // useCreateIndex: true,
+       
+
+    }).then(()=>{
         console.log("database is connected successfully");
-    }).catch(()=>{
-        console.log("DATABASE !!!ERROR");
+    }).catch((err)=>{
+        console.log("DATABASE !!!ERROR :" , err);
     })
 }
 module.exports=db
